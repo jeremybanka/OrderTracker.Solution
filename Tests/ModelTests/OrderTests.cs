cc25f3e47d17f228614f8279d18345b9ed2c5d54
Jeremy Banka <hello@jeremybanka.com>
2021-05-14 16:22:02 -0700

    ❓ add id test for orders

--- Tests/ModelTests/OrderTests.cs
+++ Tests/ModelTests/OrderTests.cs
@@ -8,18 +8,26 @@ namespace OrderTracker.Tests
   public class OrderTests
   {
     [TestMethod]
-    public void OrderCtor_MakesInstOf_Order()
+    public void Ctor_MakesInstOf_Order()
     {
       Order o = new("Bulk order for BREADs", "Yum, yum!", 5000);
       Assert.AreEqual(typeof(Order), o.GetType());
     }
 
     [TestMethod]
-    public void OrderCtor_SetsOrderDateTo_Now()
+    public void Ctor_SetsOrderDateTo_Now()
     {
       Order o = new("Bulk order for PASTRYs", "Yum, yum!", 4000);
       DateTime d = DateTime.Now;
       Assert.AreEqual(d.ToString(), o.Date.ToString());
+    }
+
+    [TestMethod]
+    public void Ctor_GivesNewOrder_ValidGuid()
+    {
+      Order o = new("", "", 0);
+
+      Assert.IsTrue(Guid.TryParse(o.Id.ToString(), out _));
     }
   }
 }
