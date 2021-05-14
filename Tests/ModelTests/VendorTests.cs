6f66e25cd1848aefe733713dc66a482b8ca31f99
Jeremy Banka <hello@jeremybanka.com>
2021-05-14 09:37:14 -0700

    ❓ test vendor for order list you can add to

--- Tests/ModelTests/VendorTests.cs
+++ Tests/ModelTests/VendorTests.cs
@@ -7,10 +7,21 @@ namespace OrderTracker.Tests
   public class VendorTests
   {
     [TestMethod]
-    public void VendorCtor_MakesInstOf_Vendor()
+    public void Ctor_MakesInstOf_Vendor()
     {
       Vendor v = new("Suzy's Bakery", "Sells plenty of tasty BREADs.");
       Assert.AreEqual(typeof(Vendor), v.GetType());
+    }
+
+    [TestMethod]
+    public void NewOrder_Increments_OrderCount()
+    {
+      Vendor v = new("Suzy's Bakery", "Sells plenty of tasty BREADs.");
+      int startingCount = v.Orders.Count;
+
+      v.NewOrder("", "", 0);
+
+      Assert.AreEqual(startingCount + 1, v.Orders.Count);
     }
   }
 }
