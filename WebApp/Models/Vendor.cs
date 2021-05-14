2a2de34efcbb5d242bf06c9c0858e978d57bf3d0
Jeremy Banka <hello@jeremybanka.com>
2021-05-14 09:38:29 -0700

    ✅ pass test; abstract creation of order as fn of vendor

--- WebApp/Models/Vendor.cs
+++ WebApp/Models/Vendor.cs
@@ -1,13 +1,21 @@
-namespace OrderTracker
+using System.Collections.Generic;
+
+namespace OrderTracker.Models
 {
   public class Vendor
   {
     public string Name { get; }
     public string Desc { get; }
+    public List<Order> Orders { get; }
     public Vendor(string name, string desc)
     {
       Name = name;
       Desc = desc;
+      Orders = new();
+    }
+    public void NewOrder(string name, string desc, int cost)
+    {
+      Orders.Add(new(name, desc, cost));
     }
   }
 }
