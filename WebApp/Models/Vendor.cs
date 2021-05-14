4f74d71529d25901da9dbe17a15453101dde0593
Jeremy Banka <hello@jeremybanka.com>
2021-05-14 12:32:54 -0700

    ✅ pass test; vendor instances may be got or cleared

--- WebApp/Models/Vendor.cs
+++ WebApp/Models/Vendor.cs
@@ -7,15 +7,19 @@ namespace OrderTracker.Models
     public string Name { get; }
     public string Desc { get; }
     public List<Order> Orders { get; }
+    private static List<Vendor> _instances = new();
     public Vendor(string name, string desc)
     {
       Name = name;
       Desc = desc;
       Orders = new();
+      _instances.Add(this);
     }
     public void NewOrder(string name, string desc, int cost)
     {
       Orders.Add(new(name, desc, cost));
     }
+    public static List<Vendor> All() => _instances;
+    public static void Purge() { _instances.Clear(); }
   }
 }
